3ba63592b34abe68290bb216aac6089e592f2947
Mackensie Bass <mab2017@live.missouristate.edu>
2022-04-18 12:46:59 -0500

    added file for minmax, began following guide for stucture

---
+++ AI540Project/minmaxAI.py
@@ -0,0 +1,8 @@
+# Following this guide for general idea on using a custom player
+# https://poke-env.readthedocs.io/en/latest/max_damage_player.html#max-damage-player
+from poke_env.player.player import Player
+
+class minMaxPlayer(Player):
+	pass
+	def choose_move(self, battle):
+		pass
