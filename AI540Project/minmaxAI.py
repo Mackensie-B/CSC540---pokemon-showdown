045c9fb38dbf100906820b578882534f77335ced
Mackensie Bass <mab2017@live.missouristate.edu>
2022-04-19 10:51:19 -0500

    minmax func start, notes about branches

--- AI540Project/minmaxAI.py
+++ AI540Project/minmaxAI.py
@@ -17,6 +17,21 @@ class minMaxPlayer(Player):
 			# some others 
 		
 		pass
+def minmax():
+# List of avilable properties for a pokemon object
+	# https://poke-env.readthedocs.io/en/latest/pokemon.html
+# move options for min max are to check types and pick best move for the type
+# tree branches:
+	####? when do we check if we are low on health?
+	# check if opponent is low on health
+		# low -> best damage move to defeat that pokemon
+		# too high
+			# check type compatibility
+				# opposing type -> choose best damage move against that type
+				####? is it possible for us to be at a big enough type 
+				##### disadvantage that we would want to switch?
+				# neutral type -> choose max damage move 
+	pass
 
 async def main():
 
